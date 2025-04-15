637b41e3950505ad0e04c4c0d382c427aec21173
David Pastor <davidpasttordev@gmail.com>
2025-04-14 21:42:21 -0600

    agregar cacnciones 2

--- src/app/components/Home/Home.tsx
+++ src/app/components/Home/Home.tsx
@@ -43,6 +43,7 @@ export default function Home() {
 
     try {
       const res = await fetch("/api/spotify/playlists", {
+        method: "GET",
         headers: { Authorization: `Bearer ${spotifyToken}` },
       });
 
